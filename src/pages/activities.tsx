c4048f8098b2108df1ee8ddb53323494481e5be7
CWCTBOY <qkrdlswo98@gmail.com>
2022-04-02 17:14:49 +0900

    image source objectify

--- src/pages/activities.tsx
+++ src/pages/activities.tsx
@@ -1,32 +1,68 @@
 import { useState } from 'react';
-import { Wrapper2, Masonry, Block, Img } from '../styles/acivities';
+import { Wrapper2, Masonry, Block, Img, Description } from '../styles/acivities';
 
-const imgSrc: string[] = [
-  'https://w0.peakpx.com/wallpaper/76/665/HD-wallpaper-wernigerode-wohltaeterbrunnen-beautiful-square-german-city-cityscape-saxony-anhalt-germany.jpg',
-  'https://images.unsplash.com/photo-1526512340740-9217d0159da9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dmVydGljYWx8ZW58MHx8MHx8&w=1000&q=80',
-  'https://i.pinimg.com/originals/d7/1c/52/d71c521ccd7baf5bd076c27ecdf769a2.jpg',
-  'https://w0.peakpx.com/wallpaper/324/696/HD-wallpaper-city-nature-sunset-toronto-vertical.jpg',
-  'https://c0.wallpaperflare.com/preview/1/703/828/fighter-jet-airshow.jpg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNecYSJLywI-eQKsTm3c4Mi5MnurISJosUnQ&usqp=CAU',
-  'https://i.pinimg.com/736x/84/af/c3/84afc369022cf56f1d8957aac5bb4aa0.jpg'
+interface ImageSrc {
+  url: string;
+  description: string;
+}
+
+const imgSrc: ImageSrc[] = [
+  {
+    url: 'https://w0.peakpx.com/wallpaper/76/665/HD-wallpaper-wernigerode-wohltaeterbrunnen-beautiful-square-german-city-cityscape-saxony-anhalt-germany.jpg',
+    description: 'Hello?'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1526512340740-9217d0159da9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dmVydGljYWx8ZW58MHx8MHx8&w=1000&q=80',
+    description: 'We'
+  },
+  {
+    url: 'https://i.pinimg.com/originals/d7/1c/52/d71c521ccd7baf5bd076c27ecdf769a2.jpg',
+    description: 'Are'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5NzY4Njc0fHxlbnwwfHx8fA%3D%3D&w=1000&q=80',
+    description: 'Network Leader'
+  },
+  {
+    url: 'https://c0.wallpaperflare.com/preview/1/703/828/fighter-jet-airshow.jpg',
+    description: 'Welcome'
+  },
+  {
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNecYSJLywI-eQKsTm3c4Mi5MnurISJosUnQ&usqp=CAU',
+    description: 'To be'
+  },
+  {
+    url: 'https://i.pinimg.com/736x/84/af/c3/84afc369022cf56f1d8957aac5bb4aa0.jpg',
+    description: 'Here'
+  },
 ]//이미지소스 데이터, 나중에 데어터 폴더생기면 분리예정
-
+// 오브젝트로 바꾸고, url,text삽입해서 수정하기 =>
 export default function Activities() {
-  const [isHover, setIsHover] = useState(false);
+  const [isTrue, setIsTrue] = useState(false);
   const onHover = () => {
-    setIsHover(!isHover);
-  }
+    setIsTrue(true);
+  };
   const onMouseLeave = () => {
-    setIsHover(!isHover);
-  }
+    setIsTrue(false);
+  };
+  console.log(isTrue)
   return (
     <Wrapper2>
       <Masonry>
         {
           imgSrc.map((item, index) => (
-            <Block key={index} onMouseEnter={onHover} onMouseLeave={onMouseLeave} >
-              <Img src={item} />
-            </Block>
+            <>
+              <Block key={index} onMouseEnter={onHover} onMouseLeave={onMouseLeave}>
+                <Description>
+                  {
+                    isTrue
+                      ? item.description
+                      : null
+                  }
+                </Description>
+                <Img src={item.url} />
+              </Block>
+            </>
           ))
         }
       </Masonry>
--- src/styles/acivities.tsx
+++ src/styles/acivities.tsx
@@ -9,10 +9,10 @@ export const Wrapper2 = styled.div`
   align-items: center;
 `
 export const Masonry = styled.div`
-  width: 900px;
+  width: 800px;
   padding: 10px;
   display: grid;
-  grid-template-rows: repeat(3,220px);
+  grid-template-rows: repeat(3,200px);
   grid-template-columns: repeat(3,1fr);
   grid-auto-rows: 100px;
 `
@@ -25,6 +25,7 @@ export const Block = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
   &:nth-of-type(1) {
     grid-column: span 2;
   }
@@ -34,10 +35,15 @@ export const Block = styled.div`
   transition: all 0.2s linear;
   &:hover {
     cursor: pointer;
-    transform: scale(1.1);
-    filter: blur(4px);
+    transform: scale(1.065);
+    filter: blur(1px);
   }
   `
 export const Img = styled.img`
-  width: 100%;
+  width: 150%;
+  `
+export const Description = styled.h2`
+  position: absolute;
+  color: whitesmoke;
+  font-size: 27px;
   `
