84e05983d22b77f2be5a481391b54d55586d1d15
CWCTBOY <qkrdlswo98@gmail.com>
2022-09-24 16:16:55 +0900

    feat: add tag atom props

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import styled from "@emotion/styled";
 
 interface TagPorps {
-  padding?: "medium";
+  padding?: "min" | "max";
   fontSize?: "small" | "xlarge" | "xxlarge";
   fontWeight?: "bold";
   color?: "black" | "highlight";
-  backgroundColor?: "block" | "purple" | "lightpurple";
+  backgroundColor?: "block" | "purple" | "lightpurple" | "purpleopacity";
   radius?: "min";
+  icon?: "add" | "remove";
   onClick?: () => void;
   children: React.ReactNode;
 }
@@ -19,11 +20,17 @@ const Tag = ({
   color,
   backgroundColor,
   radius,
+  icon,
   onClick,
   children,
 }: TagPorps) => {
   const Component = styled.span`
-    padding: ${!padding ? "5px" : "4px 25px"};
+    min-width: 10px;
+    padding: ${!padding
+      ? "4px 12px"
+      : padding === "min"
+      ? "4px 5px"
+      : "4px 20px"};
     border-radius: ${(props) =>
       !radius
         ? props.theme.borderRadius.default
@@ -39,9 +46,49 @@ const Tag = ({
     background-color: ${(props) =>
       !backgroundColor
         ? props.theme.background.black
+        : backgroundColor === "purpleopacity"
+        ? `rgba(153, 137, 247, 0.1)`
         : props.theme.background[backgroundColor]};
   `;
-  return <Component onClick={onClick}>{children}</Component>;
+  return (
+    <Component onClick={onClick}>
+      {icon === "add" ? (
+        <>
+          <span>
+            {children}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Noun_project_-_plus_round.svg/1200px-Noun_project_-_plus_round.svg.png"
+              alt="add"
+              style={{
+                width: "15px",
+                height: "15px",
+                marginLeft: "5px",
+                marginTop: "5px",
+              }}
+            />
+          </span>
+        </>
+      ) : icon === "remove" ? (
+        <>
+          <span>
+            {children}
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-256/subtract-6-451501.png"
+              alt="remove"
+              style={{
+                width: "15px",
+                height: "15px",
+                marginLeft: "5px",
+                marginTop: "5px",
+              }}
+            />
+          </span>
+        </>
+      ) : (
+        children
+      )}
+    </Component>
+  );
 };
 
 export default Tag;
