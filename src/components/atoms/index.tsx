acd3b6201630fcfa4f6fe77a566e699ae7a8e51f
Seheon Yu <seheon@seoultech.ac.kr>
2022-09-24 17:56:48 +0900

    enhance: Add atom index

---
+++ src/components/atoms/index.tsx
@@ -0,0 +1,5 @@
+export { default as Image } from './Image';
+export { default as MenuItem } from './MenuItem';
+export { default as Select } from './Select';
+export { default as Text } from './Text';
+export { default as TextField } from './TextField';
