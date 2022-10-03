1f73edf40a01ebb5e8576f822a08e42738ca9850
rlarlduf20 <kky8274@naver.com>
2022-10-03 21:18:03 +0900

    라우트 생성

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./pages";
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
