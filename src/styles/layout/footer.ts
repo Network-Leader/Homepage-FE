7ec9e526b65732493b7dd1fa0846904ca55bbb63
dwdjjj <dwdjjj@naver.com>
2022-10-14 13:57:17 +0900

    #25 header menus justiry & footer update

--- src/styles/layout/footer.ts
+++ src/styles/layout/footer.ts
@@ -1,17 +1,16 @@
 import styled from "@emotion/styled";
 
 const FooterBox = styled.div`
-  position: absolute;
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* width: 1441px; */
+  z-index: 999;
   width: 100%;
   height: 318px;
   left: 0px;
   top: 6057px;
   background-color: black;
-`
+`;
 
 const FooterIconBox = styled.div`
   display: flex;
@@ -19,13 +18,12 @@ const FooterIconBox = styled.div`
   margin-top: 148px;
   min-width: 942px;
   border-bottom: 2px solid white;
-
-`
+`;
 
 const FooterContents = styled.div`
   min-width: 942px;
   color: white;
   padding: 40px 0px;
-`
+`;
 
 export { FooterBox, FooterIconBox, FooterContents };
